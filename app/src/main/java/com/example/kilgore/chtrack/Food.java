452f8806c8c717ae68740cc47ceb59914472d946
Artur Owczarek <artur.owczarek@gmail.com>
2017-05-16 02:17:48 +0200

    added id field to Food class

--- app/src/main/java/com/example/kilgore/chtrack/Food.java
+++ app/src/main/java/com/example/kilgore/chtrack/Food.java
@@ -2,19 +2,27 @@ package com.example.kilgore.chtrack;
 
 public class Food {
 
+    private int id;
     private String name;
     private Float carb;
 
-    public Food(String name, Float carb) {
+    public Food(int id, String name, Float carb) {
+        this.id = id;
         this.name = name;
         this.carb = carb;
     }
 
+    public int getId() {
+        return id;
+    }
+
     public String getName() {
+
         return name;
     }
 
     public Float getCarb() {
+
         return carb;
     }
 }
